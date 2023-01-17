e62b0a4b18a38f02e19d19d06f372bd42eb63fcb
mallickp <prabir.mallick@gmail.com>
2023-01-17 12:24:32 +0530

    Add files via upload

---
+++ Assignment_Submission.py
@@ -0,0 +1,25 @@
+def question1(param):
+    # Write your solution here
+    # return 
+    pass
+
+def question2(param):
+    # Write your solution here
+    # return
+    pass
+
+def question3(param):
+    # Write your solution here
+    # return 
+    pass
+
+def question4(param):
+    # Write your solution here
+    # return
+    pass
+
+def question5(param):
+    # Write your solution here
+    # return 
+    pass
+
---
+++ main-Read-only/assignment_question_1.py
@@ -0,0 +1,6 @@
+import sys;
+sys.path.append('./')
+import Assignment_Submission;
+text = input()
+list = test.split(",")
+print(Assignment_Submission.question1(text))
