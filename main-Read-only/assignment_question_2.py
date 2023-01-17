f25e1f8db76b78bb42bb49ff9d99b8c89dc4ca19
mallickp <prabir.mallick@gmail.com>
2023-01-17 12:47:33 +0530

    Update assignment_question_2.py

--- main-Read-only/assignment_question_2.py
+++ main-Read-only/assignment_question_2.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.append('./')
 import Assignment_Submission;
-text = input().split(",")
-input_param = [int(a) for a in text]
-print(Assignment_Submission.question2(input_param))
+text = input()
+print(Assignment_Submission.question2(text))
