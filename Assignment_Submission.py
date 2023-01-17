cd076caad435173758ed3769b171d5fccac013cd
mallickp <prabir.mallick@gmail.com>
2023-01-17 12:29:29 +0530

    Update Assignment_Submission.py

--- Assignment_Submission.py
+++ Assignment_Submission.py
@@ -1,25 +1,10 @@
+# Given a list param = [14,12,18,63,15], write a program to return the last element.i.e in this case, output should be 15
 def question1(param):
     # Write your solution here
-    # return 
+    # return
     pass
-
+# Given a Sentence, write a program to find the number of characters in it.
 def question2(param):
     # Write your solution here
     # return
     pass
-
-def question3(param):
-    # Write your solution here
-    # return 
-    pass
-
-def question4(param):
-    # Write your solution here
-    # return
-    pass
-
-def question5(param):
-    # Write your solution here
-    # return 
-    pass
-
